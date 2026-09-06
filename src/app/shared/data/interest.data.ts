ae3eb4d42458a0f369ca8a27dd3576754edce18a
GARNIER-Emmanuel <manu.boidun@gmail.com>
2026-09-06 09:43:16 +0200

    refactor(interests): reorder interests list

--- src/app/shared/data/interest.data.ts
+++ src/app/shared/data/interest.data.ts
@@ -2,6 +2,16 @@ import { Interest } from "../interfaces";
 
 export const INTERESTS_DATA: Interest[] = [
     {
+        title: $localize`:@@interest_4_title:Cinéma`,
+        logo: "🎬",
+        description: $localize`:@@interest_4_desc:Amateur de cinéma, j'aime découvrir le cinéma en globalité, la technique derrière les films, les réalisateurs qui poussent à la réflexion et qui ne laissent aucun détail au hasard.`
+    },
+    {
+        title: $localize`:@@interest_3_title:Production Musicale`,
+        logo: "🎧",
+        description: $localize`:@@interest_3_desc:Autodidacte sur Cubase, j'écris et produis de la musique pour moi-même, explorant diverses influences et techniques.`
+    },
+    {
         title: $localize`:@@interest_1_title:Intelligence Artificielle`,
         logo: "🤖",
         description: $localize`:@@interest_1_desc:Passionné par l'IA et les modèles génératifs, je fais une veille technologique continue sur les avancées en IA, les agents autonomes et j'expérimente les nouveaux outils.`
@@ -11,14 +21,4 @@ export const INTERESTS_DATA: Interest[] = [
         logo: "🚀",
         description: $localize`:@@interest_2_desc:Intéressé par SpaceX, ArianeGroup et les défis technologiques de la conquête martienne, je m'informe sur les événements d'actualité.`
     },
-    {
-        title: $localize`:@@interest_3_title:Production Musicale`,
-        logo: "🎧",
-        description: $localize`:@@interest_3_desc:Autodidacte sur Cubase, j'écris et produis de la musique pour moi-même, explorant diverses influences et techniques.`
-    },
-    {
-        title: $localize`:@@interest_4_title:Cinéma`,
-        logo: "🎬",
-        description: $localize`:@@interest_4_desc:Amateur de cinéma, j'aime découvrir le cinéma en globalité, la technique derrière les films, les réalisateurs qui poussent à la réflexion et qui ne laissent aucun détail au hasard.`
-    }
 ];
